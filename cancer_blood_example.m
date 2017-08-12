66c1ae01f6ff9d39bc37ac22cd3ca4cd4cfa401d
Felix E. Rivera-Mariani <friveramariani@gmail.com>
2017-08-12 10:40:27 -0400

    second commit

--- cancer_blood_example.m
+++ cancer_blood_example.m
@@ -1,10 +1,15 @@
-% This code is an example of proteomics in cancer biology
+%% Example of proteomics in cancer biology with Matlab
+% This report represents a represents an example Matlab proteomic data
+% analysis. The data set analyze in this report can be found <https://home.ccr.cancer.gov/ncifdaproteomics/ppatterns.as here>
+% which is the FDA-NCI Clinical Proteomics Program Databank. The samples
+% downloaded from the FDA-NIC Proteomics Programa Databank corresponds to
+% Mass-Spec of overian cancer samples: *Cancer Group* vs  *Normal Group*. 
 
-% loading the pre-processed dataset
+%% loading the pre-processed dataset
 load OvarianCancerQAQCdataset
 whos
 
-% initializing variables to be used downstream in the data analysis
+%% initializing variables to be used downstream in the data analysis
 % workflow
 
 N = numel(grp);                         % vector of number of samples
