e5374a0d445b5b09f8cb508294e1fa9ccdbf4ee5
Felix E. Rivera-Mariani <friveramariani@gmail.com>
2017-08-12 10:29:56 -0400

    asv file

--- cancer_blood_example.m
+++ cancer_blood_example.m
@@ -1,2 +1,17 @@
 % This code is an example of proteomics in cancer biology
 
+% loading the pre-processed dataset
+load OvarianCancerQAQCdataset
+whos
+
+% initializing variables to be used downstream in the data analysis
+% workflow
+
+N = numel(grp);                         % vector of number of samples
+Cidx = strcmp('Cancer',grp);            % logical index vector for Cancer samples
+Nidx = strcmp('Normal',grp);            % logical index vector for Normal samples
+Cvec = find(Cidx);                      % index vector for Cancer samples
+Nvec = find(Nidx);                      % index vector for Normal samples
+xAxisLabel = 'Mass/Charge (M/Z)';       % x-axis label for plots
+yAxisLabel = 'Ion Intensity';           % y-axis label for plots
+
